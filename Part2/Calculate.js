b8c9a5f78f67eccd15bc3e3c5adf9393aa08b038
Chanses <chanses98@gmail.com>
2022-03-01 17:18:29 +0300

    context version

--- Part2/Calculate.js
+++ Part2/Calculate.js
@@ -5,63 +5,52 @@ function Calculate(value) {
 
   const initValue = [value];
   const signs = ["+"];
-  const methods = {
-    plus: (val) => {
-      initValue.push(val);
-      signs.push("+");
-      return methods;
-    },
-    minus: (val) => {
-      initValue.push(val);
-      signs.push("-");
-      return methods;
-    },
-    multiply: (val) => {
-      initValue.push(val);
-      signs.push("*");
-      return methods;
-    },
-    divide: (val) => {
-      initValue.push(val);
-      signs.push("/");
-      return methods;
-    },
-    calculate: () => {
-      const result = [];
-      initValue.forEach((el, index) => {
-        switch (signs[index]) {
-          case "+":
-            result.push(el);
-            break;
-          case "-":
-            result.push(-el);
-            break;
-          case "*": {
-            const lastElement = result[result.length - 1];
-            result.pop();
-            result.push(lastElement * el);
-            break;
-          }
-          case "/": {
-            const lastElement = result[result.length - 1];
-            result.pop();
-            result.push(lastElement / el);
-            break;
-          }
-        }
-      });
 
-      return result.reduce((acc, value) => acc + value);
-    },
+  this.plus = (val) => {
+    initValue.push(val);
+    signs.push("+");
+    return this;
   };
-  return methods;
+  this.minus = (val) => {
+    initValue.push(val);
+    signs.push("-");
+    return this;
+  };
+  this.multiply = (val) => {
+    initValue.push(val);
+    signs.push("*");
+    return this;
+  };
+  this.divide = (val) => {
+    initValue.push(val);
+    signs.push("/");
+    return this;
+  };
+  this.calculate = () => {
+    const result = [];
+    initValue.forEach((el, index) => {
+      switch (signs[index]) {
+        case "+":
+          result.push(el);
+          break;
+        case "-":
+          result.push(-el);
+          break;
+        case "*": {
+          const lastElement = result[result.length - 1] * el;
+          result.splice(result.length - 1, 1, lastElement);
+          break;
+        }
+        case "/": {
+          const lastElement = result[result.length - 1] / el;
+          result.splice(result.length - 1, 1, lastElement);
+          break;
+        }
+      }
+    });
+    return result.reduce((acc, value) => acc + value);
+  };
 }
 
-const check = new Calculate(5)
-  .multiply(2)
-  .plus(2)
-  .divide(1)
-  .plus(3)
-  .divide(3)
-  .calculate();
+const check = new Calculate(5).plus(2).multiply(2).multiply(2).calculate();
 console.log(check);
